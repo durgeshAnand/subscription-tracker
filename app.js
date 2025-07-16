10d4ecc3c221b607be8cf8ebc6818ad871427522
durgeshAnand <durgeshanand111@gmail.com>
2025-07-16 07:22:17 +0530

    feat: configure environment variables and update server port

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // console.log("wehjhbf gfhv");
 import express from 'express';
+import {PORT} from './config/env.js';  //importing environment variables
 
 const app = express();  //initialize app
 
@@ -7,8 +8,8 @@ app.get('/',(req,res)=>{
     res.send("welcome to subscription tracker API");
 });
 
-app.listen(3000,()=>{
-    console.log('Subscription Tracker API is running on http://localhost:3000');
+app.listen(PORT,()=>{
+    console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
 });
 
 export default app;
